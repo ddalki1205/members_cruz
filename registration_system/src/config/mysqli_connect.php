ae1f721d6c33e154fd72b3ca5420d1954552205a
ddalki <cruznikka1205@icloud.com>
2024-10-17 02:30:48 +0800

    added comments in mysqli_connect.php

--- registration_system/src/config/mysqli_connect.php
+++ registration_system/src/config/mysqli_connect.php
@@ -1,5 +1,4 @@
 <?php
-
 $host = 'localhost'; 
 $user = '201cruz'; 
 $password = 'cruz'; 
@@ -16,12 +15,24 @@ if (!$dbconnect) {
 
 /* put in http://localhost/phpmyadmin -> registration_system -> SQL query box
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     fname VARCHAR(50) NOT NULL,
     lname VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    registration_date DATETIME NOT NULL
+    registration_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (id)
 );
+
+
+ID: name of the column that stores a unique identifier for each record
+<-- why did we put ID? 1. avoid duplicates; 2. to identify each record uniquely -->
+
+INT(11): This means the id column will store an integer value. It just suggests that the number will be displayed using 11 digits,
+          but it doesn't limit the range of values, which is still the full integer range.
+
+AUTO_INCREMENT: This ensures that MySQL will automatically increase the value of id for each new row
+
+PRIMARY KEY (id): Makes the id column as the primary key of our table.
 */
 ?>
